519ed14f217b96a9585a4d9193e35773f3d3d339
[dyad] <pellizzer.emanuel@gmail.com>
2026-02-09 18:09:16 -0600

    Updated src/components/DiagnosticForm.tsx

--- src/components/DiagnosticForm.tsx
+++ src/components/DiagnosticForm.tsx
@@ -220,8 +220,8 @@ const DiagnosticForm = () => {
                     </SelectTrigger>
                     <SelectContent className="bg-slate-900 border-white/10 text-white">
                       <SelectItem value="Menos de $1M">Menos de $1M</SelectItem>
-                      <SelectItem value="1M-10M">$1M - $10M</SelectItem>
-                      <SelectItem value="10M-50M">$10M - $50M</SelectItem>
+                      <SelectItem value="$1M - $10M">$1M - $10M</SelectItem>
+                      <SelectItem value="$10M - $50M">$10M - $50M</SelectItem>
                       <SelectItem value="Más de $50M">Más de $50M</SelectItem>
                     </SelectContent>
                   </Select>
